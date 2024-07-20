ed2cb9066b6125086ddc097be3e501300825c58e
MinaEmad6060 <minaemad6060@gmail.com>
2024-07-20 09:00:13 -0700

    create ReturnRequestTableViewCell

--- Pharmacy/Pharmacy/Modules/Login/view/LoginViewController.swift
+++ Pharmacy/Pharmacy/Modules/Login/view/LoginViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var warningText: UILabel!
+    @IBOutlet weak var userNameTextFieldOutlet: UITextField!
+    @IBOutlet weak var passwordTextFieldOutlet: UITextField!
+    @IBOutlet weak var btnLoginOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        initViewController()
         
     }
+    
+    
+    
+    private func initViewController(){
+        initUserNameTextField()
+        initPasswordTextField()
+        warningText.isHidden = true
+        btnLoginOutlet.layer.cornerRadius = 16
+    }
+    
+    private func initUserNameTextField(){
+        userNameTextFieldOutlet.delegate = self
+        setBorderForView(view: userNameTextFieldOutlet)
+    }
+    
+    private func initPasswordTextField(){
+        passwordTextFieldOutlet.delegate = self
+        setBorderForView(view: passwordTextFieldOutlet)
+    }
+    
+    
+    private func setBorderForView(view: UIView){
+        view.layer.cornerRadius = 16
+        view.layer.borderWidth = 1.0
+        view.layer.borderColor = UIColor.gray.cgColor
+        view.layer.masksToBounds = true
+    }
+    
+    
 
 }
---
+++ Pharmacy/Pharmacy/View/classes/ReturnRequestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReturnRequestTableViewCell.swift
+//  Pharmacy
+//
+//  Created by Mina Emad on 20/07/2024.
+//
+
+import UIKit
+
+class ReturnRequestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
