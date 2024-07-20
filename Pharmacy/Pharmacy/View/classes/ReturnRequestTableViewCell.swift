0f96f6507b12e995a5e7ce3de22e5563322a282b
MinaEmad6060 <minaemad6060@gmail.com>
2024-07-20 09:29:25 -0700

    create ReturnRequestViewController

--- Pharmacy/Pharmacy/View/classes/ReturnRequestTableViewCell.swift
+++ Pharmacy/Pharmacy/View/classes/ReturnRequestTableViewCell.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ReturnRequestTableViewCell: UITableViewCell {
 
+    var btnShowItemsTapped: (() -> Void)?
+    
+    @IBOutlet weak var bgView: UIView!
+    
+    @IBOutlet weak var btnShowItemsOutlet: UIButton!
+    
+    @IBAction func btnShowItems(_ sender: Any) {
+        btnShowItemsTapped?()
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,7 +28,39 @@ class ReturnRequestTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        bgView.layer.cornerRadius = 16
+        bgView.layer.borderWidth = 1
+        bgView.layer.borderColor = UIColor.gray.cgColor
+
+        btnShowItemsOutlet.layer.cornerRadius = 16
+        
     }
     
 }
+/**
+ 
+
+ 
+ 
+ 
+ @IBAction func btnTripDetails(_ sender: Any) {
+     btnTripDetailsTapped?()
+ }
+ override func awakeFromNib() {
+     super.awakeFromNib()
+     // Initialization code
+ }
+
+ override func setSelected(_ selected: Bool, animated: Bool) {
+     super.setSelected(selected, animated: animated)
+     
+     bgView.layer.cornerRadius = 16
+     bgView.layer.borderWidth = 1
+     bgView.layer.borderColor = Utils.borderColor.cgColor
+     
+     btnTripDetailsOutlet.layer.cornerRadius = 16
+     Utils.setTitleForButton(btnOutlet: btnTripDetailsOutlet, key: "tripDetailsBtnTitle")
+
+     // Configure the view for the selected state
+ }
+ */
